62456f8baba5a97dfb6a1ec7dee89526e3deed2f
adamrichardson14 <45330519+adamrichardson14@users.noreply.github.com>
2021-03-18 10:39:53 +0000

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import { FC } from "react";
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return <div>This is the home page</div>;
+};
+
+export default Home;
